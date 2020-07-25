a4316de246f307189bcc3cf430dd54ffd8e2f87e
Mayanka Chattopadhyay <jodu892@gmail.com>
2020-07-26 02:33:58 +0530

    Added colors

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
    'second':0,
    'third':0
  });
+ const[hov,setHov] = React.useState(true);
  function setSlider(event)
  {
    const {name,value} = event.target;
@@ -18,7 +19,13 @@ function App() {
        [name]: value
      })
    });
+ }
 
+ function colorHover(){
+   setHov(false);
+ }
+ function colorHover1(){
+   setHov(true);
  }
 const p =[];
 Object.keys(val).forEach(function(x){
@@ -26,21 +33,25 @@ Object.keys(val).forEach(function(x){
 
 })
 
+const c = {
+  backgroundColor: 'rgb('+p[0]+','+p[1]+','+p[2]+')',
+  boxShadow : '10px 10px 40px rgba('+p[0]+','+p[1]+','+p[2]+',0.4)'
+}
+const t = {
+  backgroundColor: 'rgb('+p[0]+','+p[1]+','+p[2]+')',
+  boxShadow : '15px 15px 50px rgba('+p[0]+','+p[1]+','+p[2]+',0.45)',
+  transform: 'scale(1.01)'
+}
 
 
   return (
     <div>
       <div className="container bars">
-        <Slider SetSlider= {setSlider} Value={val.first} Name="first" Color="Red"/>
-
-        <Slider SetSlider= {setSlider} Value={val.second} Name="second" Color="Green"/>
-
-        <Slider SetSlider= {setSlider} Value={val.third} Name="third" Color="Blue"/>
-
+        <Slider SetSlider= {setSlider} Value={val.first} Name="first" Color="Red" num={val.first}/>
+        <Slider SetSlider= {setSlider} Value={val.second} Name="second" Color="Green" num={val.second}/>
+        <Slider SetSlider= {setSlider} Value={val.third} Name="third" Color="Blue" num={val.third}/>
       </div>
-      <div style={{color : 'rgb(f,g,h)'}} className="container color">
-       hello
-      </div>
+      <div style={hov?c:t} onMouseEnter={colorHover} onMouseLeave={colorHover1} className="container color" />
 
     </div>
   );
--- src/Slider.js
+++ src/Slider.js
@@ -4,10 +4,11 @@ function Slider(props)
 {
   return(
     <div className="slider-container">
-      <span className="slider">{props.Color}</span>
+      <span className="slider txt"><h3>{props.Color}</h3></span>
       <span className="slider">
       <input onChange={props.SetSlider} type="range" min="0" max="255" value={props.Value} name={props.Name}/>
       </span>
+      <span className="slider">{props.num}</span>
     </div>
   )
 }
